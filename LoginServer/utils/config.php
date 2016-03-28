57a870e5c83bcd686f8a9be2a10186ea05a24f58
feifei003603 <854535609@qq.com>
2016-03-28 11:47:33 +0800

    Create config.php

---
+++ LoginServer/utils/config.php
@@ -0,0 +1,13 @@
+
+<?php
+
+/*
+ * All database connection variables
+ */
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_NAME", "simplelogin");
+
+
